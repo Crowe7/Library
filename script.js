01a14cf2a3503b88cc537a73193892883e6c3d92
Crowe7 <jacobocrowe@gmail.com>
2021-11-26 02:30:32 -0600

    added function that assigns a number based on where it is located in array

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let myLibrary = [];
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', false);
+let myLibrary = [theHobbit];
+
+
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.hasRead = read;
+    this.indexNum = null;
   }
 
   Book.prototype.info = function() {
@@ -17,12 +21,24 @@ function Book(title, author, pages, read) {
         return this.title + " by " + this.author + ", " + this.pages  + " pages, " + this.hasRead
   }
 
-  function addBookToLibrary(e) {
+Book.prototype.index = function() {
+    for(i = 0; i < myLibrary.length; i++) {
+        if(myLibrary[i].title === this.title) {
+          return this.indexNum = i;
+            
+        }
+    }
+}
+
+function addBookToLibrary(e) {
     e.preventDefault();
      let book = makeBook();
      myLibrary.push(book);
      console.log(myLibrary);
-  }
+}
+
+theHobbit.index();
+console.log(theHobbit);
 // ADD THE EVENT LISTEN TO EACH BUTTON AS THEY ARE PUT ON
 //read as to how to set with new elements https://stackoverflow.com/questions/35349212/select-an-html-element-created-with-js-in-js
 let readButton = document.querySelector('#readToggle');
@@ -37,7 +53,7 @@ function readToggle() {
     }
   }
 
-
+// MODAL LOGIC
 let modal = document.getElementById("modalID");
 let closeButton = document.getElementById("no");
 closeButton.addEventListener('click', () => {
@@ -83,3 +99,21 @@ let submitButton = document.querySelector('#input');
 submitButton.addEventListener('submit', addBookToLibrary);
 
 //make sure to put the e.prevent deafault on whatever is called on the button at the end
+let daw = 2;
+function makeCard(){
+    let dom = document.querySelector('.card-wrapper')
+
+    let card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('id', daw);
+    dom.appendChild(card);
+
+    let container = document.createElement('div');
+    container.classList.add('container');
+    card.appendChild(container);
+
+
+
+}
+
+makeCard();
